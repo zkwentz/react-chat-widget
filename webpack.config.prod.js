babce1c4f501016150f7db2240852ecee2e51f4c
Zach Wentz <zwentz@bluebeam.com>
2020-06-21 10:40:25 -0500

    I want to see the source

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -97,10 +97,6 @@ module.exports = {
   },
   optimization: {
     minimizer: [
-      new UglifyJsPlugin({
-        cache: true,
-        parallel: true
-      }),
       new OptimizeCSSAssetsPlugin({})
     ]
   }
